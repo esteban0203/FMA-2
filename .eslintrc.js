b391c61f0df2dd81daa024e6ecfd13f84525e924
esteban0203 <esteban@brokol.app>
2025-02-16 02:07:13 -0600

    Initial version of FMA-2 with complete meal planning features, points system, and inventory management

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['universe/native'],
-  rules: {
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-unused-vars': 'warn',
-    'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
-  },
-}; 
